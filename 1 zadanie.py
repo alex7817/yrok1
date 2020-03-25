ac60e7e3348e80a754ee9f6f42ffc36564b554b9
alex7817 <ag2000@inbox.ru>
2020-03-25 17:07:22 +1000

    1 zadanie python

--- 1 zadanie.py
+++ 1 zadanie.py
@@ -1,6 +1,8 @@
-import requests
-import json
-url = 'https://WWW.cbr-xml-daily.ru/daily_json.js'
-response = requests.get(url)
-data = json.loads(response.text)
-print(data)
+# import requests
+# import json
+# url = 'https://WWW.cbr-xml-daily.ru/daily_json.js'
+# response = requests.get(url)
+# data = json.loads(response.text)
+# print(data)
+
+print("hello world")
